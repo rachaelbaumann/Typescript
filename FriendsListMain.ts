65fb57e7e0cfcbe5b5ac6266b8db0b8333342896
Rachael Baumann <baumann.rachael@gmail.com>
2018-10-17 17:34:03 -0400

    frients

--- FriendsListMain.ts
+++ FriendsListMain.ts
@@ -6,11 +6,11 @@ let emilie: FriendsList = new FriendsList("Emilie", 38, "emilie@maxtrain.com", f
 let angie: FriendsList = new FriendsList("Angie", 37, "angie@maxtrain.com", false);
 let lance: FriendsList = new FriendsList("Lance", 50, "lance@maxtrain.com", false);
 
-console.log(lance.about());
+// console.log(lance.about());
 
 let friends: FriendsList[] = [pansy, sheila, emilie, angie, lance];
 
-console.log(pansy.about());
+// console.log(pansy.about());
 
 for (let amigas of friends) {
 console.log(amigas.about());
